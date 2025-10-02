e4a5c05779f1eec492dcf31fe791c9bebf70741b
Terceiro <gustavo.f.nascimento11@aluno.senai.br>
2025-10-02 15:45:20 -0300

    first commit

---
+++ app/components/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/header.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<header>
+    <h1>Lojinha</h1>
+    <nav>
+        <a href="#">teste 1</a>
+        <a href="#">teste 2</a>
+        <a href="#">teste 3</a>
+        <a href="#">teste 4</a>
+        <a href="#">produtos</a>
+    </nav>
+</header>
--- public/index.php
+++ public/index.php
@@ -1,15 +1,28 @@
+<?php include "../app/data/produtos.php"; ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./assets/css/home.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 
 <body>
-    <h1>Hello World</h1>
+    <?php include "../app/components/header.php"; ?>
+    <div class="produtosContainer">
+        <?php foreach($produtos as $produto) : ?>
+            <div class="produto">
+                <img src="<?= $produto->imagem?>" alt="">
+                <h2><?= $produto->nome ?></h2>
+                <p><?= $produto->estoque ?></p>
+                <p><?= $produto->marca ?></p>
+                <a href="">Ver mais</a>
+            </div>
+            <?php endforeach; ?>
+            </div>
 </body>
 
 </html>
