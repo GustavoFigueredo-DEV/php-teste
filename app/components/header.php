a50b83c66bedd9c9bccde2f328ab654dd6b0d8aa
GustavoFigueredo-DEV <gustavo.f.nascimento11@aluno.senai.br>
2025-10-02 19:47:39 -0300

    adiciona estilos no header e home

--- app/components/header.php
+++ app/components/header.php
@@ -2,6 +2,9 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/header.css">
     <title>Document</title>
@@ -12,12 +15,14 @@
 </html>
 
 <header>
-    <h1>Lojinha</h1>
+    <div class="logo">
+        <img src="logo" alt="">
+    </div>
     <nav>
-        <a href="#">teste 1</a>
-        <a href="#">teste 2</a>
-        <a href="#">teste 3</a>
-        <a href="#">teste 4</a>
-        <a href="#">produtos</a>
+        <a href="#">Teste 1</a>
+        <a href="#">Teste 2</a>
+        <a href="#">Teste 3</a>
+        <a href="#">Teste 4</a>
+        <a href="#">Produtos</a>
     </nav>
 </header>
--- public/index.php
+++ public/index.php
@@ -1,28 +1,40 @@
-<?php include "../app/data/produtos.php"; ?>
+<?php 
+// Inclui o arquivo de produtos usando caminho absoluto baseado no diretório atual
+include __DIR__ . '/../app/data/produtos.php'; 
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/index.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 
 <body>
-    <?php include "../app/components/header.php"; ?>
+    <?php
+    include __DIR__ . '/../app/components/header.php'; 
+    ?>
+
     <div class="produtosContainer">
         <?php foreach($produtos as $produto) : ?>
             <div class="produto">
-                <img src="<?= $produto->imagem?>" alt="">
+                <img src="<?= $produto->imagem ?>" alt="">
+                <div class="produtoInfo">
                 <h2><?= $produto->nome ?></h2>
-                <p><?= $produto->estoque ?></p>
-                <p><?= $produto->marca ?></p>
+                <h4><?= $produto->descricao ?></h4>
+                <p>Estoque: <?= $produto->estoque ?></p>
+                <p>Marca: <?= $produto->marca ?></p>
+                </div>
                 <a href="">Ver mais</a>
             </div>
-            <?php endforeach; ?>
-            </div>
+        <?php endforeach; ?>
+    </div>
 </body>
 
 </html>
